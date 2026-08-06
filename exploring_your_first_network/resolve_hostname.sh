3b51cb45f42ed2909033dc734d8403dfe2252957
holbfr <houssemeddine.benkhalifa@holbertonschool-laval.fr>
2026-08-06 12:20:06 +0200

    Add task 4 solution

--- exploring_your_first_network/resolve_hostname.sh
+++ exploring_your_first_network/resolve_hostname.sh
@@ -1,1 +1,6 @@
 #!/usr/bin/env bash
+if [[ $# -ne 1 ]]; then
+    echo "ERROR: Wrong script usage --> You must use this script as follow: ./resolve_hostname.sh <hostname>"
+else
+    getent ahosts $1
+fi
